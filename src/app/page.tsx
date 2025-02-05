6b60c2772ef84d7d3124f8ab6a015650d1030610
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-06 01:24:11 +0600

    add nexa

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+
+const HomePage = async() => {
+
+  const res = await fetch("http://localhost:5001/blogs")
+  const blogs = await res.json()
+  console.log(blogs);
+
+  return (
+    <div className="my-10">
+   <LatestBlogs blogs={blogs}  />
+    </div>
+  );
+};
+
+export default HomePage;
