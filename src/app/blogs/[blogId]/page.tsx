0642ef2969827bf953a3681269f0fee57fadfc2a
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-06 14:52:28 +0600

    create new blog

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,10 +1,24 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async() =>{
+
+    const res = await fetch(`http://localhost:5001/blogs`);
+    const blogs = await res.json();
+
+    return blogs.slice(0,3).map((i: Blog)=> ({
+        blogId: i.id
+    }))
+}
+
 
 const BlogDetailsPage = async({params}: {params: Promise<{blogId: string}>} ) => {
 
-    const res = await fetch(`http://localhost:5001/blogs/${(await params).blogId}`)
+    const {blogId}= await params;
+
+    const res = await fetch(`http://localhost:5001/blogs/${blogId}`)
       const blog = await res.json()
-     console.log(blog);
+   
 
     return (
         <div>
---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,14 @@
+import BlogForm from '@/components/ui/BlogForm';
+import React from 'react';
+
+const CreateBlogPage = () => {
+    return (
+        <div>
+           <h1> This is blog page</h1>
+
+           <BlogForm/>
+        </div>
+    );
+};
+
+export default CreateBlogPage;
